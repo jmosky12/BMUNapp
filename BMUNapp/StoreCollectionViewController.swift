628ed16b270d43a62a82f42013ef1840799c2ab6
Michael Eliot <michael.eliot@aol.com>
2016-10-02 14:45:38 -0700

    Added Store

---
+++ BMUNapp/StoreCollectionViewController.swift
@@ -0,0 +1,98 @@
+//
+//  StoreCollectionViewController.swift
+//  BMUNapp
+//
+//  Created by Michael Eliot on 10/2/16.
+//  Copyright © 2016 Jake Moskowitz. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class StoreCollectionViewController: UICollectionViewController {
+    
+    init() {
+        super.init(nibName: "StoreCollectionViewController", bundle: nil);
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib: UINib = UINib(nibName: "StoreCollectionViewCell", bundle: nil)
+        self.collectionView!.registerNib(nib, forCellWithReuseIdentifier: "storeCell")
+        var flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: 200, height: 200)
+        flowLayout.scrollDirection = .Horizontal
+        self.collectionView?.collectionViewLayout = flowLayout
+        
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 6
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("storeCell", forIndexPath: indexPath) as! StoreCollectionViewCell
+        let label = cell.viewWithTag(1) as? UILabel
+        label!.text = "Store"
+        
+    
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+    
+}
